13e397299e1d6c71b27e530c793b626af7630fa6
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2023-04-09 20:22:02 -0700

    Adding comments

--- Server/app.py
+++ Server/app.py
@@ -8,14 +8,24 @@ app = flask.Flask(__name__)
 def index():
     return 'Hello, world!'
 
+#RPC handling and server stub
 @app.route('/<string:op>/<int:a>/<int:b>')
 def calc(op: string, a: int, b: int):
     if (op == "add"):
-        return flask.jsonify({"result": a + b}), 200
+        return flask.jsonify({"result": add(a, b)}), 200
     elif (op == "sub"):
-        return flask.jsonify({"result": a - b}), 200
+        return flask.jsonify({"result": sub(a, b)}), 200
     else: 
         return flask.jsonify({"Operation invalid": op}), 400
+
+
+#Server implementations
+def add(a: int, b: int):
+    return a + b
+
+def sub(a: int, b: int):
+    return a - b
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
